94f155d04175307cc6bf20cf4392096ca2737fe0
Braeden <braeden.norman6@gmail.com>
2023-02-11 13:20:46 -0800

    updated login page to new backend specification

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,79 +1,64 @@
 import { TextField, Button } from "@mui/material";
 import { useState } from "react";
-import { sha256 } from "js-sha256";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
-
-
-
-
-
 const Login = () => {
   const navigate = useNavigate();
-  const [serverSalt, setServerSalt] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState(null);
-  const [cookies, setCookie] = useCookies(['token']);
-
-  useState(() => {
-    fetch("http://localhost:8000/login", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => setServerSalt(data));
-  }, []);
+  const [cookies, setCookie] = useCookies(["token"]);
+  const [response, setResponse] = useState(null);
 
   const loginHandler = () => {
-    if (!username || !password) return;
+    if (!username || !password) {
+      setResponse("Please fill all the fields");
+      return;
+    }
 
-    const clientSalt = (Math.random() + 1).toString(36).substring(8);
-    const params = {
-      clientSalt: clientSalt,
-    };
     const body = {
       username: username,
-      password:  sha256(serverSalt.salt + sha256(password) + clientSalt),
+      password: password,
     };
-    fetch(
-      "http://localhost:8000/login?" + new URLSearchParams(params).toString(),
-      {
-        method: "POST",
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(body),
-      }
-    )
-      .then((res) =>  res.status === 200 ? res.json(): null)
-      .then((data) => {if (data !== null) {
-        setCookie('token', data.token, { path: '/' });
-        setCookie('user_id', data.id, { path: '/' });
-        navigate("/home", { state: data })
-  }});
+    fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(body),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.hasOwnProperty("detail")) {
+          setResponse(data.detail);
+        } else {
+          setCookie("token", data.token, { path: "/" });
+          setCookie("user_id", data.id, { path: "/" });
+          navigate("/home", { state: data });
+        }
+      });
   };
 
   return (
     <>
-      {!serverSalt && <h1>Loading....</h1>}
-      {serverSalt && (
-        <>
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button variant="contained" onClick={loginHandler}>
-            Submit
-          </Button>
-          <Link to="/registration">Click here to create account</Link>
-        </>
-      )}
+      <>
+        {response && <p>{response}</p>}
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" onClick={loginHandler}>
+          Submit
+        </Button>
+        <Link to="/registration">Click here to create account</Link>
+      </>
     </>
   );
 };
